2b28b7ba398b8393975fbbdf10149a06eb18928f
Brennan Giles <collegegiles@gmail.com>
2019-04-19 13:16:34 -0700

    update

---
+++ Items/wgsc/resources/pack-11/win-convEnv/customize.py
@@ -0,0 +1,26 @@
+# 
+
+### Begin group: DEFAULT
+ 
+# plugins
+# Installed plugins
+plugins = cPickle.loads('\x80\x02}q\x01(U\tKFX Inputq\x02U\x15plugins\\KFX Input.zipU\nKFX Outputq\x03U\x16plugins\\KFX Output.zipu.')
+ 
+# filetype mapping
+# Mapping for filetype plugins
+filetype_mapping = cPickle.loads('\x80\x02}q\x01.')
+ 
+# plugin customization
+# Local plugin customization
+plugin_customization = cPickle.loads('\x80\x02}q\x01.')
+ 
+# disabled plugins
+# Disabled plugins
+disabled_plugins = cPickle.loads('\x80\x02c__builtin__\nset\nq\x01]\x85Rq\x02.')
+ 
+# enabled plugins
+# Enabled plugins
+enabled_plugins = cPickle.loads('\x80\x02c__builtin__\nset\nq\x01]\x85Rq\x02.')
+ 
+
+
